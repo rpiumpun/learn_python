4c6e1b5376db0b3b6e0480396e93a389a5b196a5
rpiumpun <rpiumpun@gmail.com>
2021-08-13 21:55:06 +0700

    add logic gate

--- lesson 1/example_3_operators.py
+++ lesson 1/example_3_operators.py
@@ -31,3 +31,30 @@ a = 100
 
 a += 5 #same as a = a + 5
 a -= 5 #same as a = a - 5
+
+# logic gates
+j = 2
+k = 4
+
+p1 = j > k # False
+p2 = k > j # True
+
+p3 = j >= k # False
+p4 = k >= k # true
+
+p5 = j < k # True
+p6 = k < j # False
+
+p7 = j <= k # True
+p8 = j <= k # False
+
+p9 = j == k # False
+p10 = j != k # True
+
+#letter logic
+r = True
+t = False
+
+o1 = r and t # False
+o2 = r or t # True
+o3 = not t # True
