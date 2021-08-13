e6476978e18b8437357f0bf03a1256f77499839c
rpiumpun <rpiumpun@gmail.com>
2021-08-13 19:34:14 +0700

    add variable lesson

---
+++ lesson 1/example_1.py
@@ -0,0 +1,15 @@
+x = 5 
+print(x)
+
+x = 2
+print(x)
+ 
+# when run notice x has 2 values because in line 4 x overwrites as 2
+
+x = 2
+y = 3
+z = x + y
+print(z)
+
+# you can combine variable's data into something else
+# TIPS: in python you don't need to use a semicolon after writing a sentence
