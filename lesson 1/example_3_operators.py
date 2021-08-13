8a0de3a048d8d92ac00fff5ac1e32c255261a702
rpiumpun <rpiumpun@gmail.com>
2021-08-13 21:41:58 +0700

    y python cannot use var++

---
+++ lesson 1/example_3_operators.py
@@ -0,0 +1,33 @@
+# declaring var
+x = 2
+y = 5
+
+# math operators
+z1 = x + y 
+print("sum :", z1 ) #add
+
+z2 = x - y
+print("subtraction :", z2) #subtract
+
+z3 = x * y
+print("multi :", z3) #multiply
+
+z4 = x / y
+print("divide :", z4) #devide
+
+z5 = x % y
+print("remeinder :", z5) #remainder
+
+z6 = x ** y
+print("power :", z6) #power off
+
+z7 = x // y
+print("root :", z7) #squareroot
+
+z8 = (3 + 5) - 4 # using ()
+
+# math shortcut
+a = 100
+
+a += 5 #same as a = a + 5
+a -= 5 #same as a = a - 5
