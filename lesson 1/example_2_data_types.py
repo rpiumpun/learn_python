73e5179ac8b3051dcf24d1f7475a803f4fbf8137
rpiumpun <rpiumpun@gmail.com>
2021-08-13 21:23:41 +0700

    add example 2 datatypes

---
+++ lesson 1/example_2_data_types.py
@@ -0,0 +1,8 @@
+a = 1               # integers
+b = 2.22            # floats
+c = "halo world"    # strings
+d = False           # booleans
+
+x = "55" 
+y = int(x)
+print(y)            # changing string too integers 
