30ed5d1066b0553e4e001bba766c91ad26ff1562
rpiumpun <rpiumpun@gmail.com>
2021-08-14 09:57:27 +0700

    add grader app

---
+++ lesson 1/example_4_condition.py
@@ -0,0 +1,15 @@
+a = int(input("what is your score : "))
+
+# if condition
+if a >= 80:
+    print("Grade A")
+
+#elif condition (else if)
+elif a >= 70:
+    print("grade B")
+elif a >= 60:
+    print("grade C")
+
+#else condition
+else:
+    print("Fail")
